390ac729341bee92efea65a80e81c82d62008e47
Th1Alexandre <th1alexandre.dev@gmail.com>
2022-07-02 19:27:33 -0400

    update admin

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from authentication.models import TwitterUser
 
-# Register your models here.
+
+class TwitterUserAdmin(UserAdmin):
+    model = TwitterUser
+    list_display = ["id", "username", "email"]
+    list_display_links = ["id"]
+    list_per_page = 10
+
+
+admin.site.register(TwitterUser, TwitterUserAdmin)
