5001b5a4821ebbb213c7f66f2407de1263acbd37
Th1Alexandre <th1alexandre.dev@gmail.com>
2022-07-02 19:10:55 -0400

    feat: custom user model

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,24 @@
+from authentication.managers import TwitterUserManager
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from uuid import uuid4
 
-# Create your models here.
+
+class TwitterUser(AbstractUser):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False, verbose_name="UUID")
+    email = models.EmailField(unique=True, max_length=254, verbose_name='Email address')
+    name = models.CharField(max_length=50, verbose_name="Profile Name")
+    bio = models.CharField(max_length=160, blank=True, verbose_name="Bio")
+    website = models.CharField(max_length=100, blank=True, verbose_name="Website")
+    location = models.CharField(max_length=30, blank=True, verbose_name="Location")
+    birth_date = models.DateField(null=True, verbose_name="Birth Date")
+    public = models.BooleanField(default=True, help_text="Indicates whether the profile is public", verbose_name="Public")
+    following = models.IntegerField(default=0, help_text="Indicates the number of profiles following", verbose_name="Following")
+    followers = models.IntegerField(default=0, help_text="Indicates the number of followers", verbose_name="Followers")
+
+    REQUIRED_FIELDS = []
+
+    objects = TwitterUserManager()
+
+    def __str__(self):
+        return self.username
--- config/settings.py
+++ config/settings.py
@@ -107,6 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = 'authentication.TwitterUser'
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
