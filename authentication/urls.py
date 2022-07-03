49701a606a8e6013d9326d1316903b05232754c5
Th1Alexandre <th1alexandre.dev@gmail.com>
2022-07-02 20:34:21 -0400

    feat: logged user details api

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,4 +1,4 @@
-from authentication.views import RegisterAPI, LoginAPI
+from authentication.views import RegisterAPI, LoginAPI, UserAPI
 from knox import views as knox_views
 from django.urls import path
 
@@ -7,4 +7,5 @@ urlpatterns = [
     path("login/", LoginAPI.as_view(), name="login"),
     path("logout/", knox_views.LogoutView.as_view(), name="logout"),
     path("logoutall/", knox_views.LogoutAllView.as_view(), name="logoutall"),
+    path("user/", UserAPI.as_view(), name="user"),
 ]
