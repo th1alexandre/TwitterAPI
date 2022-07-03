f71ca9c6ccc688f1efe0550c3951d8eca44c344e
Th1Alexandre <th1alexandre.dev@gmail.com>
2022-07-02 23:58:02 -0400

    feat: require token authentication

--- tweet/views.py
+++ tweet/views.py
@@ -1,7 +1,11 @@
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 from tweet.serializer import TweetSerializer
 from tweet.models import Tweet
 
 class TweetsAPI(ModelViewSet):
+    permission_classes = [
+        IsAuthenticated,
+    ]
     queryset = Tweet.objects.all()
     serializer_class = TweetSerializer
